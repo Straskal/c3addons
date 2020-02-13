e71a01d14def5d7a09980201f1ea990aad4b48b9
Stephen Traskal <Straskal@gmail.com>
2020-02-12 23:10:53 -0500

    updating stateful

--- addons/Straskal_Stateful/c3runtime/instance.js
+++ addons/Straskal_Stateful/c3runtime/instance.js
@@ -5,16 +5,31 @@
         constructor(behInst, properties)
         {
             super(behInst);
-
-            this._currentState = null;
-            this._previousState = null;
-
-            this._nextState = properties[0];
+            
             this._conditions = C3.Behaviors.Straskal_Stateful.Cnds;
             this._actions = C3.Behaviors.Straskal_Stateful.Acts;
 
-            this._tickFunction = this._SwitchStateTick;
+            /**
+             * The current state and previous state of this stateful behavior.
+             */
+            this._currentState = null;
+            this._previousState = null;
 
+            /**
+             * next state is only populated if the state is changed and needs to transition.
+             */
+            this._nextState = properties[0];
+
+            /**
+             * Tick function is a 'sub state' of this behavior.
+             * 
+             * When a state change is requested, Tick function is changed to _StateTransition.
+             */
+            this._tickFunction = this._StateTransition;
+
+            /**
+             * Opt in to Tick().
+             */
             this._StartTicking();
         }
 
@@ -45,14 +60,20 @@
             }];
         }
 
-
+        /**
+         * Set the next state and switch action to transition.
+         * @param {string} state 
+         */
         _SetState(state)
         {
             this._nextState = state;
-            this._tickFunction = this._SwitchStateTick;
+            this._tickFunction = this._StateTransition;
         }
 
-        _SwitchStateTick()
+        /**
+         * Transition to the next state, invoking OnStateExit and OnStateEnter.
+         */
+        _StateTransition()
         {
             if (this._currentState)
             {
@@ -62,12 +83,13 @@
             this._previousState = this._currentState;
             this._currentState = this._nextState;
             this._nextState = null;
-
             this.Trigger(this._conditions.OnStateEnter);
-
             this._tickFunction = this._StateTick;
         }
 
+        /**
+         * Tick the current state.
+         */
         _StateTick()
         {
             this.Trigger(this._conditions.OnStateTick);
