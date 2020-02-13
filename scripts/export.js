4a7f162da1c9502c6dd3e688140d27c2384d8a63
Stephen Traskal <Straskal@gmail.com>
2020-02-12 23:00:40 -0500

    Update export.js

--- scripts/export.js
+++ scripts/export.js
@@ -4,40 +4,44 @@ const path = require('path');
 const fsExtra = require('fs-extra');
 var zipFolder = require('zip-folder');
 
-if (fs.existsSync('./addons')) {
-    fs.readdir(`./addons`, (err, items) => {
-        const files = [];
-        for (var i = 0; i < items.length; i++) {
-            let filePath = `./addons/${items[i]}`;
-            files[i] = {
-                baseName: path.basename(filePath),
-                path: filePath
-            };
-        }
-        inquirer
-            .prompt({
-                type: 'list',
-                name: 'toExport',
-                message: 'choose addon to export:',
-                choices: files.map(f => f.baseName)
-            })
-            .then(moreAnswers => {
-                const toExport = files.filter(f => f.baseName === moreAnswers.toExport)[0];
-                fsExtra.ensureDirSync('./exports');
-                zipFolder(
-                    toExport.path, `./exports/${toExport.baseName.toLowerCase().split(' ').join('-')}.c3addon`,
-                    err => {
-                        if (err) {
-                            console.log('Error occured:', err);
-                        }
-                        else {
-                            console.log('Successfully exported .c3addon: ' + path.resolve(`./exports/${toExport.baseName.toLowerCase().replace(' ', '-')}.c3addon`));
-                        }
-                    });
+fs.readdir(`./addons`, (_, items) => 
+{
+    const files = [];
+
+    for (const item of items) 
+    {
+        const filePath = `./addons/${item}`;
+
+        files.push({
+            baseName: path.basename(filePath),
+            path: filePath
+        });
+    }
+
+    inquirer.prompt({
+        type: 'list',
+        name: 'addonToExport',
+        message: 'choose addon to export:',
+        choices: files.map(f => f.baseName)
+    })
+    .then(answer => 
+    {
+        fsExtra.ensureDirSync('./exports');
+        
+        const toExport = files.filter(f => f.baseName === answer.addonToExport)[0];
+
+        zipFolder(
+            toExport.path, 
+            `./exports/${toExport.baseName.toLowerCase().split(' ').join('-')}.c3addon`,
+            err => {
+                if (err) 
+                {
+                    console.log('Error occured:', err);
+                }
+                else 
+                {
+                    console.log('Successfully exported .c3addon: ' + path.resolve(`./exports/${toExport.baseName.toLowerCase().replace(' ', '-')}.c3addon`));
+                }
             });
     });
-}
-else {
-    console.warn('there are no addons to export.');
-    console.warn('your addon files must be in a project folder named addons.');
-}
+});
